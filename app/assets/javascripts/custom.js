8e731f8c99827dc40f7e83b9332fb5bb3d6c9f8f
Michelle Golle <michelleg54@gmail.com>
2015-08-12 00:39:30 -0600

    filtering and sorting complete

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -7,18 +7,56 @@ $(document).ready(function(){
     $("#completed-task-table").toggle();
   });
 
-  $("#filter").keyup(function(){
+  $("#title-filter").keyup(function(){
       var filter = $(this).val()
-      $(".task-title").each(function(){
-        if ($(this).text().search(new RegExp(filter, "i")) < 0) {
+      if (filter == "") {
+        $(".task-title").each(function(){
+          $(this.parentElement).fadeIn();
+        });
+      } else {
+        $(".task-title").each(function(){
+          if ($(this).text().search(new RegExp(filter, "i")) < 0) {
               $(this.parentElement).fadeOut();
           } else {
               $(this).show();
           }
-      });
+        });
+      }
   });
 
+  $("#start-date-filter").keyup(function(){
+      var filter = $(this).val()
+      if (filter == "") {
+        $(".start-date").each(function(){
+          $(this.parentElement).fadeIn();
+        });
+      } else {
+        $(".start-date").each(function(){
+          if ($(this).text().search(new RegExp(filter, "i")) < 0) {
+              $(this.parentElement).fadeOut();
+          } else {
+              $(this).show();
+          }
+        });
+      }
+  });
 
+  $("#due-date-filter").keyup(function(){
+      var filter = $(this).val()
+      if (filter == "") {
+        $(".due-date").each(function(){
+          $(this.parentElement).fadeIn();
+        });
+      } else {
+        $(".due-date").each(function(){
+          if ($(this).text().search(new RegExp(filter, "i")) < 0) {
+              $(this.parentElement).fadeOut();
+          } else {
+              $(this).show();
+          }
+        });
+      }
+  });
 
   $('th').click(function(){
       var table = $(this).parents('table').eq(0)
@@ -27,25 +65,32 @@ $(document).ready(function(){
       if (!this.asc){rows = rows.reverse()}
       for (var i = 0; i < rows.length; i++){table.append(rows[i])}
   })
+
   function comparer(index) {
       return function(a, b) {
           var valA = getCellValue(a, index), valB = getCellValue(b, index)
           return $.isNumeric(valA) && $.isNumeric(valB) ? valA - valB : valA.localeCompare(valB)
       }
   }
+
   function getCellValue(row, index){ return $(row).children('td').eq(index).html() }
 
 
-  $('#filter').click(function() {
-    var filter = $(this).data("rel")
-    $(".task-tags").each(function(){
-      if ($(this).text().search(new RegExp(filter, "i")) < 0) {
-            $(this.parentElement).fadeOut();
+  $('.tag-button').click(function() {
+    var filter = $(this).text();
+    if (filter === "all") {
+      $(".task-tags").each(function(){
+        $(this.parentElement).fadeIn();
+      });
+    } else {
+      $(".task-tags").each(function(){
+        if ($(this).text().indexOf(filter) > -1) {
+          $(this).show();
         } else {
-            $(this).show();
+          $(this.parentElement).fadeOut();
         }
-    });
+      });
+    }
   });
-
 
 });
